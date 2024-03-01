fe81d3053d3279084b3b761b1b487f4398e30751
Victor Chitulescu <vchitulescu2@gmail.com>
2024-03-01 11:13:18 +0200

    push to main

---
+++ DesktopApp/APP/PizzaPlace/Program.cs
@@ -0,0 +1,16 @@
+﻿using Velopack;
+
+namespace PizzaPlace
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            VelopackApp.Build().Run();
+            var application = new PizzaPlace.App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
